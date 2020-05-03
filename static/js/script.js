f4831eea3aaca9fa0f0712eb3394382e668e8f0c
mithilanm <mithilan_26@hotmail.com>
2020-05-03 14:57:45 -0400

    Challenge 2 Update

--- static/js/script.js
+++ static/js/script.js
@@ -15,3 +15,14 @@ function ageInDays() {
 function reset() {
   document.getElementById("ageInDays").remove();
 }
+
+//Challenge 2: Cat Generator
+function generateCat() {
+  var image = document.createElement("img");
+  var div = document.getElementById("flex-cat-gen");
+  image.src =
+    "https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small";
+  div.appendChild(image);
+}
+
+//Challenge 3: Rock, Paper, Scissors
